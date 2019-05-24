99389ea86bc9a58b81697fd00bca954e4103141a
2turu <huynh.t.timothy@gmail.com>
2019-05-24 08:47:57 -0700

    script objects

---
+++ Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyCharacter : MonoBehaviour
+{
+    public string playerName;
+    public int maxHealth;
+    public int health;
+    public bool isTurn = false; //current turn
+
+    //public Skill skill1;
+    //public Skill skill2;
+
+    public Animator anim;
+
+    void Update() {
+
+    }
+
+    void BasicAttack(EnemyCharacter target, int damage) {
+
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerCharacter : ScriptableObject
+{
+    public string playerName;
+    public int maxHealth;
+    public int health;
+    public bool isTurn = false; //current turn
+
+    //public Skill skill1;
+    //public Skill skill2;
+
+    public Animator anim;
+
+    void Update() {
+
+    }
+}
